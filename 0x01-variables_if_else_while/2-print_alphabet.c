88341e9b5e9acb78c10d52aa8feb40864cee73d7
lookshines <lookshinesy2k@gmail.com>
2022-09-10 13:15:57 +0000

    print all the alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - prints all alphabet
+*
+* Description: prints all alphabet in lower case 
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 97;
+	
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+
+	return (0);
+}
