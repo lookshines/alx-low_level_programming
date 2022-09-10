a199798374eeb4f85315d3e4354c68da72fd1d55
lookshines <lookshinesy2k@gmail.com>
2022-09-10 12:32:09 +0000

    This program pint a statement if the number is negative or positve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
