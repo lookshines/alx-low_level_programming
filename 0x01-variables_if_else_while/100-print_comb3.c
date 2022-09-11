9b939d8f5eefab7876fab41d5f32a13ed7a8f23a
lookshines <lookshinesy2k@gmail.com>
2022-09-11 22:24:49 +0000

    print all combination of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - prints all combination of two digit
+*
+* Description: prints all combination two digit
+* number with , and space
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int l;
+
+	i = 48;
+	l = 49;
+	
+	while (i <= 57)
+	{
+		l = 49;
+		while (l <= 57)
+		{
+			putchar(i);
+			putchar(l);
+			if (i < 57 || l < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			l++;
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
