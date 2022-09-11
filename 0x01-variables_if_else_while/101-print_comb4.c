ac5ae1c8302b8a4797275bc7d62293ec597cad7e
lookshines <lookshinesy2k@gmail.com>
2022-09-11 22:49:55 +0000

    print all combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - prints all possible combination of three digit
+*
+* Description: prints all combination three digit
+* number with , and space
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int l;
+	int z;
+
+	i = 48;
+	l = i + 1;
+	z = l + 1;
+
+	while (i <= 57)
+	{
+		l = i + 1;
+		z = l + 1;
+		while (l <= 57)
+		{
+			z = l +1;
+			while (z <= 57)
+			{
+				putchar(i);
+				putchar(l);
+				putchar(z);
+				if (i < 55 || l < 56 || z < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				z++;
+			}	
+			l++;
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
