0623209cb93828771e37bc7468c62879cb6ab870
lookshines <lookshinesy2k@gmail.com>
2022-09-18 16:24:02 +0000

    print even fib number not greater 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+
+/**
+* main - prints the first 50 Fibonacci numbers,
+* starting with 1 and 2, followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	long fi1, fi2, curfi, sum;
+
+	i = 0;
+	fi1 = 0;
+	fi2 = 1;
+	sum = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		curfi = fi1 + fi2;
+
+		if ((curfi % 2) == 0 || curfi <= 4000000)
+			sum = sum + curfi;
+
+		fi1 = fi2;
+		fi2 = curfi;
+
+	}
+	printf("%lu\n", sum);
+	return (0);
+
+}
