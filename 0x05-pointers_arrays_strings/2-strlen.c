41dbb804cae15f42a30b0f97c26d0873efd9e4ad
lookshines <lookshinesy2k@gmail.com>
2022-09-20 05:06:03 +0000

    code correction

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,7 +6,7 @@
  *
  * Return: void
  */
-int _strlen(const char *s)
+size_t _strlen(const char *s)
 {
 	size_t len = 0;
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H 
 #define MAIN_H
+#include <stdio.h>
 
 int _putcchar(char c);
 void reset_to_98(int *n);
