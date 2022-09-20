b495e597f24de0235d360f21c822b0f343e6d778
lookshines <lookshinesy2k@gmail.com>
2022-09-20 09:40:02 +0000

    header file and function that check uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper - check for uppercase character.
+ *
+ * @c: int to check if uppercase
+ *
+ * Return 1 if uppercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More funtions and nested loop exercise
