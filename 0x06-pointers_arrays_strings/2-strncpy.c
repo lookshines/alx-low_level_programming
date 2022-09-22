98f1b188f4f5a37cc8c9e33fa7ace586e6acdfc4
lookshines <lookshinesy2k@gmail.com>
2022-09-22 16:04:36 +0000

    updating codes to while loop

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,19 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
 		dest[i] = src[i];
+		i++;
+	}
 
 
-	for (; n > i; i++)
+	while (n > i)
+	{
 		dest[i] = '\0';
-
+		i++;
+	}
 
 	return (dest);
 }
