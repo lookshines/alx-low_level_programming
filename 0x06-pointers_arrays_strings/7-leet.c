fa675bdccb75c7aa1d5cd2a309f7ce845eb9aa1f
lookshines <lookshinesy2k@gmail.com>
2022-09-22 21:11:45 +0000

    code correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,7 +13,7 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		while (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
 		while (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
