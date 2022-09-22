1654d733e18c30538cc4cf79d541c426dce8ebb7
lookshines <lookshinesy2k@gmail.com>
2022-09-22 11:36:31 +0000

    correct code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,17 +15,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	
-	while (n > i)
+	for (; n > i; i++)
 	{	
 		dest[i] = '\0';
-		i++;
 	}
-	
 	return (dest);
 }
