ac87ea4f6a7efad5278f66e0c87d53b821c85dd3
lookshines <lookshinesy2k@gmail.com>
2022-09-22 11:31:16 +0000

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ *
+ * @dest: 1st string
+ * @src: 2nd stringi
+ * @n: number of character to copy
+ *
+ * Return: char
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	
+	while (n > i)
+	{	
+		dest[i] = '\0';
+		i++;
+	}
+	
+	return (dest);
+}
