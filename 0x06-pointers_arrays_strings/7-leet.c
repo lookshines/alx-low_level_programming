e408b301bd4f5209d385e932ae748b1790ae3588
lookshines <lookshinesy2k@gmail.com>
2022-09-23 04:47:33 +0000

    code correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,28 +9,18 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, l;
+	char *m = "aeotl";
+	char *M = "AEOTL";
+	int x[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		
-		
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		
-		
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		
-		
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		
-		
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (l = 0; l < 5; l++)
+		{
+			if (s[i] == m[l] || s[i] == M[l])
+				s[i] = x[l];
+		}
 	}
 
 	return (s);
