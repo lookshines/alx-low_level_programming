bb3a4a8a1b258872e9fb9889291db6c9548875f3
lookshines <lookshinesy2k@gmail.com>
2022-09-27 19:49:00 +0000

    code correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,11 +13,13 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-			return(s + i);
+			return (&s[i]);
 	}
-
-	return ('\0');
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
 }
