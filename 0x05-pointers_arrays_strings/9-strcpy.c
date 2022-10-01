092a22f601ac785ed9a3c90288e9f3424c377d5a
lookshines <lookshinesy2k@gmail.com>
2022-10-01 11:09:35 +0000

    code correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,17 +7,30 @@
  * @dest: The buffer to copy string to
  * @src: The source to copy
  *
- * Return: void
+ * Return: copied outputi 
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, n;
+
+	n = 0;
+	while (src[n] != '\0')
+	{
+		n++;
+	}
 
 	i = 0;
-	while (src[i])
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
+	}
+
+	i = i;
+	while (i <= n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 
