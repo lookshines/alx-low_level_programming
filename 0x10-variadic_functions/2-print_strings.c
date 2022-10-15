2073fd8bb8b838558040df6ba11894dc94985325
lookshines <lookshinesy2k@gmail.com>
2022-10-15 13:39:11 +0000

    code updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,7 +28,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
-	va_end(string);
 	printf("\n");
+	
+	va_end(string);
 
 }
