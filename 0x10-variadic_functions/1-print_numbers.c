a3d4ea2609454191dbbd696045396ac281a18a11
lookshines <lookshinesy2k@gmail.com>
2022-10-15 13:06:13 +0000

    unction that prints numbers, followed by a new line.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print all numbers in the parameter
+ * @separator: the character that seprate the numbers
+ * @n: number of parameter provided
+ * @...: variable number of parameters
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list nums;
+
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (separator != NULL && i != (n - 1))
+			printf(", ");
+	}
+	va_end(nums);
+	printf("\n");
+
+}
