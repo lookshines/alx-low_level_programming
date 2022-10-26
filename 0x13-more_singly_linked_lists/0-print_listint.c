ef369267231393cb5152d23c927aa605a993a520
lookshines <lookshinesy2k@gmail.com>
2022-10-26 04:27:34 +0000

    unction that prints all the elements of a link list structure

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all element of a listint_t list
+ * @h: listint_t linklist
+ *
+ * Return: the number of nodes
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *nodes = h;
+	size_t i = 0;
+
+	while (nodes)
+	{
+		printf("%i\n", nodes->n);
+		i++;
+		nodes = nodes->next;
+	}
+
+	return (i);
+}
