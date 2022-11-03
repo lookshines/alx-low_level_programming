13d545283b1e6fe393b94590ed7cfb6b27a05dc2
lookshines <lookshinesy2k@gmail.com>
2022-11-03 05:03:19 +0000

    update code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int op, wr, len = 0;
 
 	/*Check the file*/
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	/*Get String lenght*/
