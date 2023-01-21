8124917f1628b6329e4254c2ff4241b0eacca0b6
lookshines <lookshinesy2k@gmail.com>
2023-01-21 18:43:34 +0000

    function that deletes the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -45,4 +45,7 @@ int sum_listint(listint_t *head);
 
 /*function that inserts a new node at a given position.*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
+/*function that deletes the node at index index of a listint_t linked list.*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 #endif
