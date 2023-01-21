82ad876daa89f3994ac92a0a20a1172a73d9c023
lookshines <lookshinesy2k@gmail.com>
2023-01-21 18:32:10 +0000

    updated code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,14 +14,14 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *h;
 	listint_t *new;
+	listint_t *h;
 
 	h = *head;
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx && h != NULL; i++)
+		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
 			h = h->next;
 		}
